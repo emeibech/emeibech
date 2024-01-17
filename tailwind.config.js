8f627cff0691221b5f56d7e7e790b4f2ffda169d
emeibech <98073805+emeibech@users.noreply.github.com>
2024-01-18 03:02:33 +0800

    Adjust tailwind config and import custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: '360px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['outfit', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      colors: {
+        bglight: '#f8ecef',
+        bgdark: '#12060a',
+        fglight: '#000',
+        fgdark: '#fff'
+      }
+    },
   },
   plugins: [],
 }
