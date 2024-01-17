78c932bfeb7a5422b4226e078e70b1d5c093604d
emeibech <98073805+emeibech@users.noreply.github.com>
2024-01-18 03:18:32 +0800

    Create basic structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div
+      className={`min-h-screen bg-bglight dark:bg-bgdark
+       text-fglight dark:text-fgdark`}
+    >
+      <main className="min-h-screen grid place-content-center">
+        <div
+          className={`
+          cursor-default font-medium flex flex-col gap-2 px-4
+          text-center md:text-start
+        `}
+        >
+          <h1 className="fluidtext-4xl">Mark Anthony Bechayda</h1>
+          <h2 className="fluidtext-2xl text-[#b94062] dark:text-[#e662bf]">
+            Fullstack Web Developer
+          </h2>
+        </div>
+      </main>
+      <footer></footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
